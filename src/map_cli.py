34f7eb24a63bb678bdcaf013ef37d33e65fee616
Martin Wellman <mwellman@ohri.ca>
2024-09-30 12:42:06 -0700

    Turn off ruff formating for class opts

--- src/map_cli.py
+++ src/map_cli.py
@@ -22,27 +22,28 @@ from utils.cli_utils import get_input_data_files
 
 if __name__ == "__main__":
     if "get_ipython" in globals():
-
+        # fmt: off
         class opts:
             # ODM v1 to v2
-            module = "odm_v1_to_v2"
-            module_dir = None
-            input_data_dir = "../../../PHES-ODM-Data/odm_v1_data/centreau_qc/updated"
-            input_data_files = None  # ["WWMeasure", "../../../PHES-ODM-Data/odm_v1_data/centreau_qc/updated/WWMeasure.csv"]
-            output_dir = "../gen/odm_v1_to_v2"
-            temp_dir = "../gen/odm_v1_to_v2/temp"
+            # module = "odm_v1_to_v2"
+            # module_dir = None
+            # input_data_dir = "../../../PHES-ODM-Data/odm_v1_data/centreau_qc/updated"
+            # input_data_files = None  # ["WWMeasure", "../../../PHES-ODM-Data/odm_v1_data/centreau_qc/updated/WWMeasure.csv"]
+            # output_dir = "../gen/odm_v1_to_v2"
+            # temp_dir = "../gen/odm_v1_to_v2/temp"
 
             # NWSS to v2
-            # module = "nwss_reporting_to_v2"
-            # module_dir = None
-            # input_data_dir = "../../../PHES-ODM-Data/nwss/private_renamed/"
-            # input_data_files = None  # ["nwss", "../../../PHES-ODM-Data/nwss/private_renamed/nwss[cdc-nwss-restricted-data-set-wastewater-2024-03-19].csv"]
-            # output_dir = "../gen/nwss_reporting_to_v2"
-            # temp_dir = "../gen/nwss_reporting_to_v2/temp"
+            module = "nwss_reporting_to_v2"
+            module_dir = None
+            input_data_dir = "../../../PHES-ODM-Data/nwss/private_renamed/"
+            input_data_files = None # [ "nwss", "../../../PHES-ODM-Data/nwss/private_renamed/nwss[cdc-nwss-restricted-data-set-wastewater-2024-03-19].csv" ]
+            output_dir = "../gen/nwss_reporting_to_v2"
+            temp_dir = "../gen/nwss_reporting_to_v2/temp"
 
             max_processes = 1
             input_max_rows = 50
-            id_debug = False
+            id_debug = True
+        # fmt: on
     else:
         args = argparse.ArgumentParser(
             formatter_class=argparse.ArgumentDefaultsHelpFormatter
