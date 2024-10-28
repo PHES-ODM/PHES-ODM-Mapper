a679bcbedba6a5c23fd173f064a2dc9d54684ef6
Martin Wellman <mwellman@ohri.ca>
2024-10-28 12:00:30 -0700

    index_in_progress to tag IDValues as being calculated, for detecting circular ID dependencies

--- src/id_generator/id_value.py
+++ src/id_generator/id_value.py
@@ -3,6 +3,19 @@
 Holds a custom-generated ID. Has an option to add an index to the ID, which allows to generate primary keys that are
 unique, in case there are conflicting primary key values.
 
+The index_in_progress member should be set to True if the caller is currently calculating the index for
+the IDValue object. Once we are finished calculating the index it should be set to False. This is to avoid
+circular dependencies on the IDs. Calculating the index of a primary key IDValue requires calculating all the other IDs
+in the current row of the table. This is so that we know if the row is a duplicate of another row (in which case
+we would use the index from one of the duplicate rows), or not a duplicate (in which case we add a new unique
+index). While calculating the rest of the row we may require using an IDValue where the index is being calculated,
+and so we cannot know what the string value of the index is yet. If this occurs, then an exception is raised.
+
+To reduce the risk of the above problem, the index is sometimes removed/stripped when using an IDValue to calculate another
+IDValue, for example, fn.makeid(datEmpty.datasets.datasetID, datEmpty.sites.siteID) will use the datasetID
+and the siteID WITHOUT the index value included, so it doesn't matter if the index is being calculated
+or not.
+
 # Usage
 
 ```python
@@ -24,7 +37,9 @@ from typing import Optional
 
 
 class IDValue(object):
-    def __init__(self, root_id: Optional[str], index: int = 0):
+    def __init__(
+        self, root_id: Optional[str], index: int = 0, index_in_progress: bool = False
+    ):
         """Constructor for IDValue
 
         Args:
@@ -34,6 +49,8 @@ class IDValue(object):
                 keys are unique and do not conflict with other primary keys. If 0 then no index is added after
                 the ID. To get the full string of the ID with the index pass the IDValue object to the str() function
                 (ie. str(idvalue_obj)). Defaults to 0.
+            index_in_progress (bool, optional): Set to True if the caller is currently calculating the index for
+                this object, False if the caller isn't calculating the index.
         """
         self._root_id: Optional[str] = root_id
         self._index: int = index
@@ -42,6 +59,7 @@ class IDValue(object):
         # We calculate these values on-demand as an optimization
         self._str_value: Optional[str] = None
         self._repr_value: Optional[str] = None
+        self._index_in_progress: bool = index_in_progress
 
     def __str__(self) -> str:
         if self._str_value is None:
@@ -51,7 +69,9 @@ class IDValue(object):
 
     def __repr__(self) -> str:
         if self._repr_value is None:
-            self._repr_value = f"<IDValue:{str(self)}>"
+            self._repr_value = (
+                f"<{type(self).__name__}:{str(self)} object at {hex(id(self))}>"
+            )
         return self._repr_value
 
     def __eq__(self, value: object) -> bool:
@@ -89,3 +109,21 @@ class IDValue(object):
             int: The integer index of the ID.
         """
         return self._index
+
+    @property
+    def index_in_progress(self) -> bool:
+        """Determine if we are currently calculating the index of this ID.
+
+        Returns:
+            bool: True if we are currently calculating the index of this ID, False otherwise.
+        """
+        return self._index_in_progress
+
+    @index_in_progress.setter
+    def index_in_progress(self, index_in_progress: bool):
+        """Set if we are calculating the index of this ID or not.
+
+        Args:
+            index_in_progress (bool): True if we are calculating the index, False if we are not.
+        """
+        self._index_in_progress = index_in_progress
