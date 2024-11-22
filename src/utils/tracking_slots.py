365000304035d048cf9325c0b22ce086e53b80c9
Martin Wellman <mwellman@ohri.ca>
2024-11-22 12:52:36 -0700

    Move load_data_with_tracking_columns to tracking_slots.py

--- src/utils/tracking_slots.py
+++ src/utils/tracking_slots.py
@@ -1,10 +1,26 @@
-from typing import Dict, List, Union
+import sys
+import os
+
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), ".."))
+
+from typing import Dict, List, Union, Optional
 
 from pathlib import Path
 import pandas as pd
 
 from linkml_runtime import SchemaView
 from linkml_runtime.linkml_model import SlotDefinition
+
+from utils.clean_exit_error import CleanExitError
+from utils.schema_utils import (
+    all_classes_without_tree_root,
+    validate_columns_with_schema,
+)
+from progress import ProgressCounter, EmptyCounter
+from utils.general_utils import read_data_frame, EXCEL_FILE_KEY, EXCEL_SHEET_KEY
+from utils.logger import get_logger
+
+logger = get_logger(__name__)
 
 
 # All tracking slots. These slots are added to the data to map before mapping occurs.
@@ -130,3 +146,126 @@ def add_tracking_columns(df: pd.DataFrame, class_name: str, file: Union[str, Pat
     else:
         # The above fails if df is empty, so properly handle it here.
         df[TrackingSlots.SOURCE_FILE_AND_ROW] = None
+
+
+def load_data_with_tracking_columns(
+    data_files: Dict[str, List[Union[str, Path, Dict[str, str]]]],
+    schema: Union[SchemaView, str, Path] = None,
+    max_rows: Optional[int] = 0,
+    random_sample_data: bool = False,
+    progress_id: Optional[str] = None,
+    add_all_tracking_columns: bool = False,
+    validate_class_names: bool = False,
+    validate_columns: bool = False,
+) -> Dict[str, List[pd.DataFrame]]:
+    """Load all data from disk (as DataFrames) and add the tracking columns.
+
+    Args:
+        data_files (Dict[str, List[Union[str, Path]]]): Dictionary of all files to load. The keys are the class
+            names and the values are lists of files belonging to that class.
+        source_schema_file (Union[str, Path]): The source schema that contains the classes that the data_files
+            should belong to. Only files belonging to recognized classes are loaded.
+        max_rows (Optional[int], optional): Maximum number of rows to load from each file. If 0 or None then all
+            rows are loaded. Defaults to 0.
+
+    Returns:
+        Dict[str, List[pd.DataFrame]]: The loaded DataFrames. The keys are the class names and the values
+            are lists of DataFrames belonging to that class. The order of the DataFrames within each class are the
+            same as the order of the files in data_files for the same class.
+    """
+    if not data_files:
+        raise CleanExitError("No input data found.")
+
+    if isinstance(schema, (str, Path)):
+        schema = SchemaView(schema)
+
+    if schema is not None:
+        recognized_classes = all_classes_without_tree_root(schema)
+    else:
+        recognized_classes = None
+
+    # Check for invalid class names
+    if validate_class_names and schema is not None:
+        has_unrecognized_class = False
+        for class_name, files in data_files.items():
+            if class_name not in recognized_classes:
+                has_unrecognized_class = True
+                for file in files:
+                    logger.error(f"Unrecognized input table '{class_name}': {file}")
+        if has_unrecognized_class:
+            tables = ", ".join(sorted(recognized_classes, key=lambda x: str(x).lower()))
+            msg = f"Terminating due to unrecognized table(s). Allowable tables are: {tables}"
+            raise CleanExitError(msg)
+
+    total_items = sum([len(d) for d in data_files.values()])
+    if progress_id:
+        progress = ProgressCounter({progress_id: total_items}, multiple_bars=False)
+    else:
+        progress = EmptyCounter()
+
+    warning_log = []
+    with progress:
+        data_frames = {}
+        for class_name, files in data_files.items():
+            if validate_class_names and recognized_classes is not None:
+                if class_name not in recognized_classes:
+                    # Unrecognized class name, so ignore the file (but tell the user)
+                    for file in files:
+                        logger.info(
+                            f"Ignoring file from unrecognized table '{class_name}': {file}"
+                        )
+                        progress.update(progress_id, 1)
+                    continue
+            if class_name not in data_frames:
+                data_frames[class_name] = []
+            for file in files:
+                try:
+                    if isinstance(file, Dict):
+                        track_file = f"{file[EXCEL_FILE_KEY]}:{file[EXCEL_SHEET_KEY]}"
+                    else:
+                        track_file = file
+                    read_kwargs = {
+                        "nrows": None
+                        if random_sample_data
+                        else (max_rows if max_rows else None),
+                        "keep_default_na": False,
+                        "na_values": None,
+                    }
+                    df = read_data_frame(file=file, **read_kwargs)
+                except pd.errors.EmptyDataError:
+                    logger.warning(
+                        f"Empty file found for table '{class_name}', ignoring: {track_file}"
+                    )
+                    df = None
+                except FileNotFoundError:
+                    raise CleanExitError(f"Specified file does not exist: {track_file}")
+
+                if df is not None:
+                    if validate_columns and schema is not None:
+                        new_log = validate_columns_with_schema(
+                            df, schema=schema, class_name=class_name, file=track_file
+                        )
+                        warning_log.extend(new_log)
+
+                    # Add tracking columns
+                    if add_all_tracking_columns:
+                        add_tracking_columns(df, class_name, track_file)
+
+                    data_frames[class_name].append(df)
+
+                    logger.info(
+                        f"Loaded {len(df)} rows for table '{class_name}': {track_file}"
+                    )
+
+                progress.update(progress_id, 1)
+
+    if warning_log:
+        for msg in warning_log:
+            logger.warning(msg)
+
+    if len(data_frames) == 0:
+        tables = ", ".join(sorted(recognized_classes))
+        msg = f"No recognized tables loaded. Allowable tables are: {tables}"
+        raise CleanExitError(msg)
+
+    return data_frames
