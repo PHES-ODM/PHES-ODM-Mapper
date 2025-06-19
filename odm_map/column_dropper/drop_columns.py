dc19338be403dfb3a761ea3a83afc6516fba0571
Martin Wellman <mwellman@ohri.ca>
2025-06-19 13:43:00 -0700

    Add drop_columns action.

---
+++ odm_map/column_dropper/drop_columns.py
@@ -0,0 +1,112 @@
+# %%
+"""
+Drop columns from DataFrames, either loaded from disk or already in memory. The columns that
+get dropped can include:
+
+1) Extra columns. These are columns that begin with the string _extra_. They are typically added
+in an upstream mapping operation.
+2) Tracking columns. These are columns that specify which row number and class/table that a row in
+a DataFrame was populated from. They are typically added in an upstream mapping operation.
+3) Any column that is not a recognized column according to a LinkML schema.
+
+## Usage
+
+```python
+data_files = {
+    "measures": ["measures1.csv", "measures2.csv"],
+    "measureSets": ["measureSets1.csv", measureSets2.csv"],
+    # etc.
+}
+drop = DropColumns()
+drop.drop_columns(
+    data_files=data_files,
+    data_frames=None,
+    drop_extra_columns=False,
+    drop_tracking_columns=False,
+    keep_columns_in_schema_only=True,
+    output_dir="dropped/data",
+    max_rows=0,
+    schema="schemas/odm_v3.yaml",
+)
+```
+"""
+
+from typing import Dict, List, Union, Optional
+from pathlib import Path
+import pandas as pd
+from linkml_runtime import SchemaView
+
+from odm_map.utils.logger import get_logger
+from odm_map.utils.extra_and_tracking_slots import is_extra_slot, is_tracking_slot
+from odm_map.utils.general_utils import (
+    load_data_frames_for_classes,
+    save_data_frames_for_classes,
+)
+
+logger = get_logger(__name__)
+
+
+class DropColumns:
+    def __init__(self):
+        pass
+
+    def drop_columns(
+        self,
+        data_files: Dict[str, List[Union[str, Path, Dict]]],
+        data_frames: Dict[str, List[pd.DataFrame]],
+        drop_extra_columns: bool = False,
+        drop_tracking_columns: bool = False,
+        keep_columns_in_schema_only: bool = False,
+        output_dir: Optional[Union[str, Path]] = None,
+        max_rows: Optional[int] = None,
+        schema: Optional[Union[SchemaView, Path, str]] = None,
+    ) -> Dict[str, List[pd.DataFrame]]:
+        if isinstance(schema, (Path, str)):
+            sv: SchemaView = SchemaView(schema)
+        else:
+            sv: SchemaView = schema
+
+        if not data_frames:
+            data_frames = {}
+
+        # Load files from disk
+        if data_files:
+            logger.info("Loading data files to drop columns from")
+            load_data_frames_for_classes(data_files, data_frames, max_rows=max_rows)
+
+        # Go through all the classes in data_frames
+        for class_name, dfs in data_frames.items():
+            # Go through all the DataFrames for the current class
+            logger.info(f"Dropping columns from class {class_name}")
+            for idx in range(len(dfs)):
+                df = dfs[idx]
+
+                if keep_columns_in_schema_only:
+                    # Only keep the columns that are recognized in the LinkML schema
+                    if sv is None:
+                        raise ValueError(
+                            "Schema must be specified if keep_columns_in_schema_only is True, but None was given."
+                        )
+                    slot_defns = sv.class_induced_slots(class_name)
+                    slots = [s.name for s in slot_defns]
+                    df = df[slots]
+                else:
+                    # Gather all columns to drop in drop_cols (extra and/or tracking columns)
+                    drop_cols = []
+                    if drop_extra_columns:
+                        drop_cols.extend([c for c in df.columns if is_extra_slot(c)])
+                    if drop_tracking_columns:
+                        drop_cols.extend([c for c in df.columns if is_tracking_slot(c)])
+                    if drop_cols:
+                        # Drop the columns in drop_cols
+                        df = df[[c for c in df.columns if c not in drop_cols]]
+
+                # Save the DataFrame to return
+                dfs[idx] = df
+
+        if output_dir:
+            save_data_frames_for_classes(
+                data_frames, output_dir, file_name_format="{class_name}.csv"
+            )
+
+        return data_frames
---
+++ odm_map/column_dropper/drop_columns_cli.py
@@ -0,0 +1,95 @@
+# %%
+
+from typing import Annotated, List, Optional
+from pathlib import Path
+import typer
+
+from odm_map.column_dropper.drop_columns import DropColumns
+from odm_map.utils.cli_utils import get_input_data_files
+
+app = typer.Typer(
+    pretty_exceptions_show_locals=False,
+    rich_markup_mode="rich",
+)
+
+MAIN_HELP = """Drop columns from tabular data. The dropped columns can be configured from
+the command line. Options include dropping extra columns (columns starting with _extra_), dropping
+tracking columns (columns that specify which row and file/table the row was populated from),
+and dropping columns that are not part of the class according to a LinkML schema."""
+
+INPUTS_HELP = """All the data files to drop columns from. The class the data file
+              belongs to is determined by the file's name. eg. measures.csv belongs
+              to the measures class, and samples[2025-07-01].csv belongs to the
+              samples class."""
+
+DROP_EXTRA_COLUMNS_HELP = """If set then drop the extra columns. These are columns that
+                          begin with the string '_extra_'."""
+
+DROP_TRACKING_COLUMNS_HELP = """If set then drop the tracking columns. These are columns
+                             that specify from which row number and class/table the current
+                             row was populated from during a mapping operation."""
+
+KEEP_COLUMNS_IN_SCHEMA_ONLY_HELP = """If set then only keep the columns that are recognized
+                                   for the class according to the schema specified by the
+                                   --schema option."""
+
+OUTPUT_DIR_HELP = """Directory to save the results to."""
+
+SCHEMA_HELP = """If --keep-columns-in-schema-only is specified, then this is the LinkML schema
+              used to determine which columns to keep. If --keep-columns-in-schema-only is not
+              specified then --schema is not required."""
+
+MAX_ROWS_HELP = """Maximum number of rows to load from each input data file. If not set
+                or 0 then all rows are loaded."""
+
+
+@app.command(help=MAIN_HELP)
+def main(
+    inputs: Annotated[List[Path], typer.Argument(show_default=False, help=INPUTS_HELP)],
+    output_dir: Annotated[
+        Path, typer.Argument(show_default=False, help=OUTPUT_DIR_HELP)
+    ],
+    drop_extra_columns: Annotated[
+        bool, typer.Option(help=DROP_EXTRA_COLUMNS_HELP)
+    ] = False,
+    drop_tracking_columns: Annotated[
+        bool, typer.Option(help=DROP_TRACKING_COLUMNS_HELP)
+    ] = False,
+    keep_columns_in_schema_only: Annotated[
+        bool, typer.Option(help=KEEP_COLUMNS_IN_SCHEMA_ONLY_HELP)
+    ] = False,
+    schema: Annotated[Path, typer.Option(help=SCHEMA_HELP)] = None,
+    max_rows: Annotated[Optional[int], typer.Option(help=MAX_ROWS_HELP)] = None,
+):
+    data_files = get_input_data_files(inputs, schema=schema)
+
+    drop = DropColumns()
+    drop.drop_columns(
+        data_files=data_files,
+        data_frames=None,
+        drop_extra_columns=drop_extra_columns,
+        drop_tracking_columns=drop_tracking_columns,
+        keep_columns_in_schema_only=keep_columns_in_schema_only,
+        output_dir=output_dir,
+        max_rows=max_rows,
+        schema=schema,
+    )
+
+
+if __name__ == "__main__":
+    if "get_ipython" in globals():
+        opts = {
+            "inputs": [
+                "/Users/martinwellman/Documents/Health/Wastewater/PHES-ODM-Mapper/PHES-ODM-Mapper/gen/pha4ge-to-v2/measures.csv",
+                "/Users/martinwellman/Documents/Health/Wastewater/PHES-ODM-Mapper/PHES-ODM-Mapper/gen/pha4ge-to-v2/measureSets.csv",
+            ],
+            "drop_extra_columns": False,
+            "drop_tracking_columns": False,
+            "keep_columns_in_schema_only": True,
+            "output_dir": "/Users/martinwellman/Documents/Health/Wastewater/PHES-ODM-Mapper/PHES-ODM-Mapper/gen/pha4ge-to-v2/dropped",
+            "max_rows": None,
+            "schema": "/Users/martinwellman/Documents/Health/Wastewater/PHES-ODM-Mapper/PHES-ODM-Mapper/odm_map/data/modules/pha4ge-to-v2/schemas/odm_v3.yaml",
+        }
+        main(**opts)
+    else:
+        app()
