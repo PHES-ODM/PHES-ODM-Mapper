04b06f15fba407277cac8554f26cb4dee8c73b95
Martin Wellman <mwellman@ohri.ca>
2025-10-10 14:41:46 -0700

    Allow get_slot_definition to throw an exception or not if class/slot doesn't exist. Change get_ranges_of_slot/get_ranges_of_slot_defn to extract ranges from "any_of" field of LinkML schemas.

--- odm_map/utils/schema_utils.py
+++ odm_map/utils/schema_utils.py
@@ -4,11 +4,11 @@ Utility functions for LinkML schemas.
 
 from typing import Dict, List, Union, Optional
 from dataclasses import asdict
-import yaml
 from pathlib import Path
 import numpy as np
 
 from linkml_runtime import SchemaView
+from linkml_runtime.linkml_model import SlotDefinition
 
 from odm_map.utils.logger import make_logger_bullet_list
 from odm_map.utils.general_utils import (
@@ -68,7 +68,9 @@ def all_classes_without_tree_root(schema: SchemaView) -> List[str]:
     return classes
 
 
-def get_slot_definition(cls: str, slot: str, schema: SchemaView) -> Dict:
+def get_slot_definition(
+    cls: str, slot: str, schema: SchemaView, exception_on_error: bool = True
+) -> Dict:
     """Get the full definition for the slot. This includes fields that are attributes of the class.
     If a slot is modified with a slot_usage, then we also update the returned dictionary with the
     slot usage information.
@@ -77,42 +79,100 @@ def get_slot_definition(cls: str, slot: str, schema: SchemaView) -> Dict:
         cls (str): The class that contains the slot.
         slot (str): The slot name to get the definition for.
         schema (SchemaView): The Schema the class and slot belong to.
+        exception_on_error (bool): If True then raise an exception if the slot does not exist. If False then
+            return None if the slot does not exist.
 
     Returns:
         Dict: The dictionary with all information about the slot (eg. the name, range, pattern, etc).
             If the slot is not a member of the class then None is returned.
     """
+    if exception_on_error:
+        return asdict(schema.induced_slot(slot, cls))
+
     class_definition = schema.induced_class(cls)
     if slot in class_definition.attributes:
         return asdict(class_definition.attributes[slot])
     return None
 
 
-def get_ranges_of_slot(cls: str, slot: str, schema: SchemaView) -> List[str]:
-    """Get the range(s) (if any) of the slot in the specified class.
+def get_ranges_of_slot(
+    class_name: str,
+    slot_name: Union[str, List[str]],
+    schema: SchemaView,
+    exception_on_error: bool = True,
+) -> List[str]:
+    """Get the range(s) (if any) of the slot(s) in the specified class.
 
     Args:
         cls (str): The class that the slot belongs to.
-        slot (str): The slot to get the range for.
+        slot (Union[str, List[str]]): The slot(s) to get the range(s) for.
         schema (SchemaView): The Schema to retrieve the slot info from.
+        exception_on_error (bool): If True then raise an exception if the a slot does not exist. If False then
+            an empty range is retrieved for slots that do not exist.
 
     Returns:
-        List[str]: A list of range(s) for the specified slot, if at least one range exists. If
-            no range is found (eg. the class or slot are invalid) then None is returned.
+        List[str]: A list of range(s) for the specified slots, if at least one range exists. If
+            no range is found (eg. the class or slot are invalid) then an empty list is returned.
     """
-    defn = get_slot_definition(cls, slot, schema)
+    if isinstance(slot_name, str):
+        slot_name = [slot_name]
+    ranges = []
+    for cur_slot in slot_name:
+        # slot_defn = schema.induced_slot(cur_slot, class_name)
+        slot_defn = get_slot_definition(
+            class_name, cur_slot, schema, exception_on_error=exception_on_error
+        )
+        if slot_defn:
+            cur_ranges = get_ranges_of_slot_defn(slot_defn)
+            if cur_ranges:
+                ranges.extend(cur_ranges)
 
-    if defn is not None:
-        defn = defn.get("range", None)
-        if defn is not None:
-            # defn is of type linkml_runtime.linkml_model.meta.ElementName
+    # Remove duplicates (but retain order)
+    ranges = list(dict.fromkeys(ranges))
+    return ranges
+
+
+def get_ranges_of_slot_defn(
+    slot_defn: Union[Dict, SlotDefinition, List[SlotDefinition]],
+) -> List[str]:
+    """Get the range(s) (if any) of the slot definition(s).
+
+    Args:
+        slot_defn (Union[Dict, SlotDefinition, List[SlotDefinition]]): The SlotDefinition(s) to get the ranges of.
+
+    Returns:
+        List[str]: A list of range(s) for the specified slot(s), if at least one range exists. If
+            no range is found then an empty list is returned.
+    """
+    if isinstance(slot_defn, (SlotDefinition, Dict)):
+        slot_defn = [slot_defn]
+    ranges = []
+    for cur_defn in slot_defn:
+        if not isinstance(cur_defn, dict):
+            cur_defn = asdict(cur_defn)
+        # Try getting the range
+        range_defn = cur_defn.get("range", None)
+        if range_defn is not None:
+            # range_defn is of type linkml_runtime.linkml_model.meta.ElementName
             # We need to convert it to either type str or type List[str]
-            defn = yaml.safe_load(str(defn))
+            cur_ranges = [str(range_defn)]
 
-    if isinstance(defn, str):
-        defn = [defn]
+        # Try getting any_of
+        any_of_defn = cur_defn.get("any_of", None)
+        if any_of_defn is not None:
+            any_of_ranges = []
+            for cur_defn in any_of_defn:
+                cur_range = cur_defn.get("range", None)
+                if cur_range:
+                    any_of_ranges.append(str(cur_range))
+            if len(any_of_ranges):
+                cur_ranges = any_of_ranges
 
-    return defn
+        ranges.extend(cur_ranges)
+
+    # Remove duplicates (but retain order)
+    ranges = list(dict.fromkeys(ranges))
+    return ranges
 
 
 def validate_columns_with_schema(
