e2016b7519fbaa07dae121ebd736cfe81bab8b81
Martin Wellman <mwellman@ohri.ca>
2025-11-04 14:03:10 -0700

    Changed tables_to_shortnames value in config file to be automatically generated from ODM data dictionary (in make_wide_to_long_config_cli.py), instead of hard-coded in template config file.

--- odm_map/prepare_wide_to_long/make_wide_to_long_config_cli.py
+++ odm_map/prepare_wide_to_long/make_wide_to_long_config_cli.py
@@ -7,6 +7,7 @@ import os
 
 from odm_map.utils.general_utils import read_data_frame
 from odm_map.utils.logger import get_logger
+from odm_map.prepare_wide_to_long.wide_column_utils import ConfigKeys
 
 MAIN_HELP = """Generate the configuration file needed for the
 WideColumnExpander to run for ODM wide tables."""
@@ -46,26 +47,32 @@ class MakeWideToLongConfig:
             self.config_template = {}
 
     def make(self, output_file: Union[str, Path]):
-        # 1. Select all rows in data dictionary where mmaSet is not NA
-        # 2. The partID maps onto that mmaSet
-        #     part_id: mma_set
-        # mma_sets_df = self.df[self.df[]]
-        # print(output_file)
         self.config = {}
         self.config.update(self.config_template)
 
+        # When setting protocolSteps.method or protocolSteps.measure, the value we set
+        # will determine which enumeration values protocolSteps.value can take on.
+        # These enumeration sets (called mmaSets for "measure, method, attribute sets") are subsets
+        # of the larger "methods" and "measurements" sets. The mapping partid_to_mmaset
+        # tells us which mmaSet to use for a given method/measure value.
         # Select all rows in data dictionary where mmaSet is not NA
         mma_sets_df = self.df[~pd.isna(self.df["mmaSet"])]
         # Select the rows where partType is any of ["methods", "measurements"]
         mma_sets_df = mma_sets_df[
             mma_sets_df["partType"].isin(["methods", "measurements"])
         ]
-
         # Each partID (which are enumeration values) gets mapped onto the mmaSet
         partid_to_mmaset = {}
         for _, row in mma_sets_df[["partID", "mmaSet"]].iterrows():
             partid_to_mmaset[row[row.index[0]]] = row[row.index[1]]
-        self.config["partid_to_mmaset"] = partid_to_mmaset
+        self.config[ConfigKeys.PARTID_TO_MMASET] = partid_to_mmaset
+
+        # Get all the tables, and their shortcodes stored in partInstr
+        tables_df = self.df[self.df["partType"] == "tables"]
+        tables_to_shortnames = {}
+        for _, row in tables_df[["partID", "partInstr"]].iterrows():
+            tables_to_shortnames[row["partID"]] = row["partInstr"]
+        self.config[ConfigKeys.TABLES_TO_SHORTNAMES] = tables_to_shortnames
 
         # Save config to disk
         logger.info(f"Saving to {output_file}")
